99ab97fa3f48d8240cc488f0633959ddeff9e52b
unknown <__prashant@bitbucket.com>
2019-09-28 15:48:46 +0530

    print the wierd series is done

---
+++ epi-7.8.js
@@ -0,0 +1,45 @@
+// takes a string and says it, means 1 => 11 (one 1) 21 => 1211(one 2 one 1) 111223 => 312213(three 1 , two 2, one 3)
+
+let says = (num) => {
+  let toScan = num.toString().split('');
+  let count = 1;
+  let result = [];
+
+  const addToResult = (count, num) => {
+    result.push(count);
+    result.push(num);
+  }
+
+  for (i = 0; i < toScan.length; i++) {
+    if (i !== 0 && toScan[i] === toScan[i - 1]) {
+      count = count + 1;
+    }
+
+    if (i !== 0 && toScan[i] !== toScan[i - 1]) {
+      addToResult(count, toScan[i - 1]);
+      count = 1;
+    }
+
+    if (i === toScan.length - 1) {
+      addToResult(count, toScan[i]);
+      count = 1;
+    }
+  }
+
+  return result.join('');
+}
+
+// i will just call it a key that reprasants the nth number
+let getMyKey = (num) => {
+  if (num === 1) {
+    return 1;
+  }
+
+  prev = 1;
+  for (j = 2; j <= num; j++) {
+    prev = says(prev);
+    if (j === num) {
+      return prev;
+    }
+  }
+}
